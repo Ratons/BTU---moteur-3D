90a78ba829084358cff154b6ffe3779ef4acac70
Ratons <kaelig.lemener@gmail.com>
2020-10-12 15:20:33 +0200

    déplacement

---
+++ BTU- Moteur 3D/Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Camera m_MainCamera;
+    [SerializeField] int m_VerticalSpeed;
+    [SerializeField] int m_HorizontalSpeed;
+
+
+    //Controle du vaisseau
+    void PlayerControl()
+    {
+        //Horizontal
+        if(Input.GetAxis("Horizontal") < 0 && 
+            m_MainCamera.WorldToScreenPoint(transform.position).x > m_HorizontalSpeed * Time.deltaTime)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate(new Vector3(horizontalInput, 0, 0) * m_HorizontalSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Horizontal") > 0 &&
+            m_MainCamera.WorldToScreenPoint(transform.position).x < Screen.width - m_HorizontalSpeed * Time.deltaTime)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate(new Vector3(horizontalInput, 0, 0) * m_HorizontalSpeed * Time.deltaTime);
+        }
+        //Vertical
+        if (Input.GetAxis("Vertical") < 0 &&
+            m_MainCamera.WorldToScreenPoint(transform.position).y > m_VerticalSpeed * Time.deltaTime)
+        {
+            float verticalInput = Input.GetAxis("Vertical");
+            transform.Translate(new Vector3(0, verticalInput, 0) * m_HorizontalSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Vertical") > 0 &&
+                m_MainCamera.WorldToScreenPoint(transform.position).y < Screen.height - m_VerticalSpeed * Time.deltaTime)
+        {
+            float verticalInput = Input.GetAxis("Vertical");
+            transform.Translate(new Vector3(0, verticalInput, 0) * m_HorizontalSpeed * Time.deltaTime);
+        }
+        /*
+        if(Input.GetAxis("Fire1") && m_fireTimer.ElapsedMilliseconds > m_FireRate)
+        {
+            Bullet bullet = Instantiate(m_BulletPrefab).GetComponent<bullet>();
+            bullet.transform.position = transform.position;
+            bullet.OnHit += OnBulletHit;
+
+            m_FireTimer.Restart();
+        }*/
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerControl();
+    }
+}
