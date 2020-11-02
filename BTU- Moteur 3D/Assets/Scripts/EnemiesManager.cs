51676a02be67a9b70e7fd12c8ea1079f30ea1321
xelaced <alexis.decaux@yahoo.fr>
2020-11-02 14:54:39 +0100

    enemies

---
+++ BTU- Moteur 3D/Assets/Scripts/EnemiesManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesManager : MonoBehaviour
+{
+    [SerializeField] Enemy m_enemy;
+    [SerializeField] float m_spawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemySpawner());
+    }
+
+    IEnumerator EnemySpawner()
+    {
+        while (true)
+        {
+            //SpawnEnemy
+            if (Application.isPlaying)
+                Instantiate(m_enemy, this.transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(m_spawnTime);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BTU- Moteur 3D/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float m_enemySpeed;
+    // Start is called before the first frame update
+    void EnemyControl()
+    {
+        if(this.transform.position.y <= -2)
+            Destroy(this.gameObject);
+        else
+            transform.Translate(new Vector3(0, -1, 0) * m_enemySpeed * Time.deltaTime);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyControl();
+    }
+}
