085573f23d5e48d202542908081058f006c7599d
Ratons <kaelig.lemener@gmail.com>
2020-11-10 09:47:26 +0100

    changement boutons start menu + ajout ui stats (non fonctionnel)

---
+++ BTU- Moteur 3D/Assets/Stat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat : MonoBehaviour
+{
+    public Player player;
+
+    public GameObject AttackText;
+    public static int Attack = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attack = 10;
+
+    }
+}
